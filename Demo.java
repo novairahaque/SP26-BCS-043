98dee7c2070edd1f46fd838d9b066412b9e66f77
novairahaque <novairahaque@gmail.com>
2026-09-16 17:56:16 +0500

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -1,11 +1,31 @@
 public class Demo{
     public static void main(String[] args) {
-       Student s = new Student();
-    
-       s.id = 043;
-       s.name = "Zamil";
-       s.compCredits = 4;
+       Student s1 = new Student();
+       Student s2 = new Student();
 
-       s.printSummary();
-  }
+       s1.id = 015;
+       s1.name = "Usman";
+       s1.compCredits = 16;
+
+       s2.id = 016;
+       s2.name = "Sana";
+       s2.compCredits = 18;
+
+       System.out.println("Before changes: ");
+
+       s1.printSummary();
+       s2.printSummary();
+
+       s1.name = "Ali";
+       s1.compCredits = 17;
+       
+       s2.addCredits(10);
+
+       
+       System.out.println("After adding credits to s2: ");   
+
+       s1.printSummary();
+       s2.printSummary();
+
+         }
     }
