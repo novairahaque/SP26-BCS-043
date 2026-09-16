5de565560c7117fe71b7b8f635c315503ee2c431
novairahaque <novairahaque@gmail.com>
2026-09-16 17:57:10 +0500

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -1,27 +1,8 @@
 public class Demo {
     public static void main(String[] args) {
 
-        Student a = new Student();
-        a.name = "Ali";
+        Student currentStudent = null;
 
-        Student b = a;
-        b.name = "Sara";
-
-        Student c = new Student();
-        c.name = a.name;
-
-        System.out.println(a.name);
-        System.out.println(b.name);
-        System.out.println(c.name);
-
-        System.out.println(a == b);
-        System.out.println(a == c);
-
-        b = new Student();
-        b.name = "Amna";
-
-        System.out.println(a.name);
-        System.out.println(b.name);
-        System.out.println(c.name);
+        currentStudent.printSummary();
     }
 }
